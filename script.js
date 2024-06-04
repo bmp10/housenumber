303358ccc0b86ac1a78d7ca73b431725d8569cb9
BMP <89558241+bmp10@users.noreply.github.com>
2024-06-03 18:55:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ let output = document.createElement('p')
 document.body.appendChild(output)
 
 input.oninput = function() {
-    output.innerText = parseInt(input.value) % 80
+    let val = parseInt(input.value)
+    let sumofdigits = val
+        .toString()
+        .split('')
+        .map(Number)
+        .reduce(function (a, b) {
+            return a + b;
+        }, 0);
+    
+    let rem = val % 80
+    let sumsum = sumofdigits + rem
+
+    output.innerText = rem + '\n' + sumofdigits + '\n' + sumsum
 }
